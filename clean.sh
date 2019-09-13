c52c10b9dbdbcfe47dd0899e384ed00b39bcb544
Daniel Moberg <moberg@beboplogin4.lcrc.anl.gov>
2019-09-13 14:23:57 -0500

    added more potentials, cleaned up cmake and tests, began adding Tinker compatibility

--- clean.sh
+++ clean.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 rm -r build/*
-
-#rm -r Makefile CMakeCache.txt cmake_install.cmake CMakeFiles/
-#cd src/
-#rm -r Makefile CMakeCache.txt cmake_install.cmake CMakeFiles/
-#cd ../pot/
-#rm -r Makefile CMakeCache.txt cmake_install.cmake CMakeFiles/
+rm -r exe/
 
