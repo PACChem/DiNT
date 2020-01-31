28498bd3c0f037cfe6afe290a405c69734c19c4f
mobergd <mobergdr@gmail.com>
2020-01-31 11:55:23 -0600

    updated CMakeLists, compilation compatibility w/Theta

--- compile.sh
+++ compile.sh
@@ -2,9 +2,8 @@
 
 #rm -r build/*
 cd build/
-#cmake -DCMAKE_Fortran_FLAGS=" -O3 -g -L/home/ajasper/KTP/dint/sprng/lib -L/usr/lib64 -L/home/moberg/lib -lmlfg" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_INSTALL_PREFIX=../ ../
 cmake -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_INSTALL_PREFIX=../ ../
-make
+make VERBOSE=1
 make install
 
 #cd src/
